0ee13bd50db0ea6e8156a6905237a24cb84060a2
shaikkhajaibrahim <shaikkhajaibrahim@live.in>
2025-01-19 08:42:50 +0530

    Added module

--- azure/vnet/outputs.tf
+++ azure/vnet/outputs.tf
@@ -1,6 +1,6 @@
-output "subnet_ids" {
-    value = azurerm_subnet.subnets[*].id
-}
+# output "subnet_ids" {
+#     value = azurerm_subnet.subnets[*].id
+# }
 
 output "vnet_id" {
     value = azurerm_virtual_network.base.id
